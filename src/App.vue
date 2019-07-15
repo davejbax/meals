99695d33ae5750d4bda10c59fba26461d56b6387
davejbax <davejbax@gmail.com>
2019-07-15 13:31:19 +0100

    Add meal component and basic data structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="grid">
+    
+    <section class="grid__main">
+      <ul class="meal-list">
+        <li
+          v-for="meal in meals"
+          v-bind:key="meal.id"
+        >
+          <Meal
+            v-bind:meal="meal"
+            v-bind:items="getItems(meal.id)"
+          />
+        </li>
+      </ul>
+    </section>
+
+    <aside class="grid__sidebar">
+      TODO
+    </aside>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Meal from './components/Meal.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Meal
+  },
+  data() {
+    return {
+      meals: [
+        {
+          id: 0,
+          name: 'Breakfast'
+        },
+        {
+          id: 1,
+          name: 'Lunch'
+        },
+        {
+          id: 2,
+          name: 'Tea'
+        }
+      ],
+
+      foods: [
+        {
+          id: 0,
+          name: 'Porridge',
+          nutrition: {
+            energy: 500,
+            protein: 10,
+            carbs: 10,
+            fat: 10
+          },
+          servingName: 'bowl'
+        }
+      ],
+
+      plans: [
+        {
+          id: 0,
+          name: 'Monday',
+          items: [
+            {
+              mealId: 0,
+              foodId: 0,
+              foodQuantity: 1
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    getItems(mealId) {
+      // TODO: non-const plan ID
+      let items = this.plans[0].items
+        // Filter to find only items concerning the correct meal
+        .filter(item => item.mealId === mealId)
+
+        // Map to an object of { food: Food, quantity: number },
+        // for easy processing
+        .map(item => {
+          return {
+            food: this.foods.find(food => food.id === item.foodId),
+            quantity: item.foodQuantity
+          }
+        });
+      
+      return items;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/**
+ * App-wide styles
+ */
+
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  background: #e5e5e5;
+  font-family: 'Roboto', Arial, sans-serif;
+}
+
+/**
+ * Reusable styles
+ */
+
+.inline {
+  display: inline-block;
+}
+
+.card {
+  padding: 16px;
+  border-radius: 3px;
+
+  background: white;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
+}
+
+.card__title {
+  padding: 0;
+  margin: 0;
+  margin-bottom: 16px;
+
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.card__subtitle {
+  color: #0097A7;
+  font-weight: 500;
+}
+
+.card__subtitle.inline {
+  margin-left: 8px;
+}
+
+/**
+ * Component-specific styles
+ */
+
+.grid {
+  display: flex;
+}
+
+.grid__main {
+  flex: 2;
+}
+
+.grid__sidebar {
+  flex: 1;
+  flex-shrink: 0;
+}
+
+.meal-list {
+  list-style: none;
+  padding: 0;
+  margin: 16px;
+}
+
+.meal-list li {
+  margin-bottom: 16px;
+}
+
+.meal-list li:last-child {
+  margin-bottom: 0;
 }
 </style>
---
+++ src/components/Meal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <div>
+      <h5 class="card__title inline">{{ meal.name }}</h5>
+      <span class="card__subtitle inline">500</span>
+    </div>
+    <table class="meal">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Food</th>
+          <th>Energy (kcal)</th>
+          <th>Carbohydrates</th>
+          <th>Protein</th>
+          <th>Fat</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, index) in items"
+          v-bind:key="index"
+        >
+          <td>{{ item.quantity }}</td>
+          <td>{{ item.food.name }}</td>
+          <td>{{ item.quantity * item.food.nutrition.energy }}</td>
+          <td>{{ item.quantity * item.food.nutrition.carbs }}</td>
+          <td>{{ item.quantity * item.food.nutrition.protein }}</td>
+          <td>{{ item.quantity * item.food.nutrition.fat }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Meal',
+  props: ['meal', 'items']
+}
+</script>
+
+<style>
+.meal {
+  width: 100%;
+
+  padding: 0;
+  border-collapse: collapse;
+}
+
+.meal thead {
+  font-size: 0.9em;
+  color: #777;
+}
+
+.meal tr {
+  height: 48px;
+  
+  border-bottom: 1px solid #DDD;
+}
+
+.meal tbody tr:hover {
+  background: #EEE;
+}
+
+.meal td,
+.meal th {
+  padding: 0 16px;
+}
+</style>
